b54fde302fe7ba37bf94d5d77ec6e77e1b3ca7ad
gailole <32515761+gailole@users.noreply.github.com>
2025-06-16 14:01:31 +0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,10 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+
+  // 👇 добавь эту строку
+  base: '/final_myapp/',
+
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
